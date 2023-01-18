91ae8d2d38f8abd870ccd181690ed50d6a522308
shawnthestudent <90001513+shawnthestudent@users.noreply.github.com>
2023-01-18 10:28:23 -0500

    removed comment

--- linecount.cc
+++ linecount.cc
@@ -2,12 +2,6 @@
 #include <fstream>
 using namespace std;
 
-// output format to use IDENTICALLY:
-//BSH:Saru> ./filesize input.3lines input.1line 
-//program: ./filesize
-// input.3lines: 3
-// input.1line: 1
-
 int main( int argc, char* argv[] )
 {
     // just to get you started, this is how to refer to the arguments that were passed
